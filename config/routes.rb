462f38c7d1e9f6beb446614e8f54baceb84fcee1
sspatari <spatari.stanislav@gmail.com>
2019-04-29 23:00:04 +0300

    implement tods controller and change todos resource scope in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'todos/index'
-  get 'todos/create'
-  get 'todos/update'
-  get 'todos/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api/v1' do
+    resources :todos
+  end
 end
