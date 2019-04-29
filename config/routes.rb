31bfdcdc7729171dfc94b11edee525d5dd11e74e
sspatari <spatari.stanislav@gmail.com>
2019-04-29 22:06:46 +0300

    add todo model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'todos/index'
+  get 'todos/create'
+  get 'todos/update'
+  get 'todos/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
