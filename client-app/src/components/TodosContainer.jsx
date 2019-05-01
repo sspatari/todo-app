0ec4e4c4ff60d0e139dd56eb04daf46da198159e
sspatari <spatari.stanislav@gmail.com>
2019-05-01 19:47:25 +0300

    implement getTodos and update Todos container render method to render all fetched todos

--- client-app/src/components/TodosContainer.jsx
+++ client-app/src/components/TodosContainer.jsx
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class TodosContaiter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  getTodos() {
+    axios
+      .get("api/v1/todos")
+      .then(response => {
+        this.setState({ todos: response.data });
+      })
+      .catch(error => console.log(error));
+  }
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
   render() {
     return (
       <div>
@@ -10,10 +31,21 @@ class TodosContaiter extends Component {
             type="text"
             placeholder="Add a task"
             maxLength="50"
+            onKeyPress={this.createTodo}
           />
         </div>
         <div className="listWrapper">
-          <ul className="taskList" />
+          <ul className="taskList">
+            {this.state.todos.map(todo => {
+              return (
+                <li className="task" todo={todo} key={todo.id}>
+                  <input className="taskCheckbox" type="checkbox" />
+                  <label className="taskLabel">{todo.title}</label>
+                  <span className="deleteTaskBtn">x</span>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     );
